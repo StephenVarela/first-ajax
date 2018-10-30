d7342966cbb09e84c7db32247ffa8fe95aea820c
StephenVarela <varelast92@gmail.com>
2018-10-30 15:33:16 -0400

    added get to ping and appended text to parent element of the button

--- ajax.js
+++ ajax.js
@@ -9,6 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+  var pingPongAJAX = document.getElementById('AJAXToPingPong');
+
+  pingPongAJAX.addEventListener('click', function(e){
+    axios({
+      "url": 'http://intro-ajax-api.herokuapp.com/ping',
+      "dataType": 'text'
+    }).then(function (response) {
+      console.log(response.data)
+      pingPongAJAX.parentElement.append(response.data);
+    });
+  });
+
 
 
 });
